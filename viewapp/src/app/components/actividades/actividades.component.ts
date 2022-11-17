396718dece077550482c7c4a8b256ce7b60db7eb
valexx55 <valexx55@gmail.com>
2022-11-17 18:54:29 +0100

    ocio listado actividades

--- viewapp/src/app/components/actividades/actividades.component.ts
+++ viewapp/src/app/components/actividades/actividades.component.ts
@@ -1,4 +1,7 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Observer } from 'rxjs';
+import { Evento } from 'src/app/models/evento';
 import { MadridService } from 'src/app/services/madrid.service';
 
 @Component({
@@ -10,9 +13,30 @@ export class ActividadesComponent implements OnInit {
 
 
   distritos:Array<string>;
+  //public madridActivitites?: any[];
+  public madridActivitites?: Array<Evento>;
+  private districtObserver: Observer<any>;
 
   constructor(private servicioMadrid:MadridService) {
     this.distritos = ['ARGANZUELA', 'BARAJAS', 'CARABANCHEL','CENTRO','CHAMARTIN','CHAMBERI', 'CIUDAD LINEAL', 'FUENCARRAL-EL PARDO', 'HORTALEZA', 'LATINA', 'MONCLOA-ARAVACA', 'MORATALAZ', 'PUENTE DE VALLECAS', 'RETIRO', 'SALAMANCA', 'SAN BLAS-CANILLEJAS', 'TETUAN', 'USERA', 'VILLA DE VALLECAS', 'VILLAVERDE' ];
+    this.districtObserver = {
+      next: (districtObserver: any) => {
+        console.log('Repsuesta distrito recibido', districtObserver);
+        //districtObserver.@graph//como no puedo acceder así,
+        this.madridActivitites = <Array<Evento>> districtObserver['@graph'];//ojo con la forma de accesso alternativa
+        this.madridActivitites.forEach(
+          (evento_actual:Evento) => 
+          {
+            console.log(`TITULO =  ${evento_actual.title}` );
+            console.log(`LATITUD =  ${evento_actual.location.latitude}` );
+            console.log(`LONGITUD =  ${evento_actual.location.longitude}` );
+          }
+        )
+      },
+      error: (error: HttpErrorResponse) =>
+        console.error('Error al recibir el distrito', error),
+      complete: () => console.log('Completado llamada a distrito'),
+    };
    }
 
   ngOnInit(): void {
@@ -22,7 +46,11 @@ export class ActividadesComponent implements OnInit {
   obtenerEventos (distrito_seleccionado:string)
   {
     //TODO: obtenemos los eventos de un distrito
-  }
+    this.servicioMadrid
+    .getDistrictEvents(distrito_seleccionado)
+    .subscribe(this.districtObserver);
+}
+  
   distritoSeleccionado(evento:Event)
   {
     console.log("distrito seleccionado");
