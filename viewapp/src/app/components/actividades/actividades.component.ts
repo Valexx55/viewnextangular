408596b484de105304ce6f8890fc817f88170017
valexx55 <valexx55@gmail.com>
2022-11-17 17:36:15 +0100

    actividades madrid 2/3

--- viewapp/src/app/components/actividades/actividades.component.ts
+++ viewapp/src/app/components/actividades/actividades.component.ts
@@ -8,15 +8,29 @@ import { MadridService } from 'src/app/services/madrid.service';
 })
 export class ActividadesComponent implements OnInit {
 
-  constructor(private servicioMadrid:MadridService) { }
+
+  distritos:Array<string>;
+
+  constructor(private servicioMadrid:MadridService) {
+    this.distritos = ['ARGANZUELA', 'BARAJAS', 'CARABANCHEL','CENTRO','CHAMARTIN','CHAMBERI', 'CIUDAD LINEAL', 'FUENCARRAL-EL PARDO', 'HORTALEZA', 'LATINA', 'MONCLOA-ARAVACA', 'MORATALAZ', 'PUENTE DE VALLECAS', 'RETIRO', 'SALAMANCA', 'SAN BLAS-CANILLEJAS', 'TETUAN', 'USERA', 'VILLA DE VALLECAS', 'VILLAVERDE' ];
+   }
 
   ngOnInit(): void {
-    
+    console.log(this.distritos.length);
   }
 
-  obtenerEventos ()
+  obtenerEventos (distrito_seleccionado:string)
   {
     //TODO: obtenemos los eventos de un distrito
+  }
+  distritoSeleccionado(evento:Event)
+  {
+    console.log("distrito seleccionado");
+    let elemento_select: HTMLSelectElement = <HTMLSelectElement>evento.target;
+    //elemento_select: HTMLSelectElement = evento.target as HTMLSelectElement;
+
+    console.log("distrinto seleccionado = "+elemento_select.value);
+    this.obtenerEventos(elemento_select.value);
   }
 
 }
