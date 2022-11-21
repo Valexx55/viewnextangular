341164e58ea910e318b80f83d807871454c01205
valexx55 <valexx55@gmail.com>
2022-11-21 16:02:42 +0100

    mapa con posicion

--- viewapp/src/app/components/mapa/mapa.component.ts
+++ viewapp/src/app/components/mapa/mapa.component.ts
@@ -35,7 +35,52 @@ export class MapaComponent implements OnInit, AfterViewInit {
   }
 
 
+  encuentrame()
+  {
+    //vamos a ubicar al usuario con su posición geográfica
+    //si existe el api
+    if (navigator.geolocation)
+    {
+      console.log("El api existe en este navegador");
+      navigator.geolocation.getCurrentPosition((pos) => {this.exito(pos);} , ()=>{this.fracaso();})
+    } else {
+      console.log("El api NO existe en este navegador");
+      this.fracaso();
+    }
 
+    
+      //pedir el acceso a la ubicación
+    //si no, mostramos un mensajillo de error
+  }
+
+  fracaso ()
+    {
+     alert("No es posbile determinar su ubicación en este dispositivo"); 
+    }
+
+    dibujarPosicionEnElMapa (latitud, longitud)
+    {
+////USO EL API DE LEAFLET //https://leafletjs.com/examples/quick-start/
+      let nivel_de_zoom = 12;
+
+      this.map.setView([latitud, longitud], nivel_de_zoom);
+
+      var circle = L.circle([latitud, longitud],
+        {
+          color: 'blue',
+          fillColor: '#000',
+          fillOpacity: 0.5,
+          radius: 50
+        }).addTo(this.map);
+    }
+
+    exito (pos:GeolocationPosition)
+    {
+     //alert("No es posbile determinar su ubicación en este dispositivo"); 
+     console.log(` SU POSICIÓN ES latitud ${pos.coords.latitude} longuitud ${pos.coords.longitude}`);
+     this.dibujarPosicionEnElMapa(pos.coords.latitude, pos.coords.longitude);
+
+    }
 
   
 
